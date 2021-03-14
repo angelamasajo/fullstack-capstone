6eed7ccc9e1e7813711518f521219655be66d73a
Angela Masajo <aabmasajo@gmail.com>
2021-03-13 19:55:38 -0600

    all tests work

--- src/PlantList/PlantList.test.js
+++ src/PlantList/PlantList.test.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PlantList from './PlantList'
+import React from "react";
+import ReactDOM from "react-dom";
+import PlantList from "./PlantList";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PlantList />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<PlantList allPlants={[]} />, div);
   ReactDOM.unmountComponentAtNode(div);
-}) 
+});
