08f71e14f9bb393f6b47618b83aa70199757bf0c
Angela Masajo <aabmasajo@gmail.com>
2021-03-18 01:34:39 -0500

    home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
 
@@ -21,21 +21,12 @@ class Home extends Component {
           Plantventory allows users to browse through available plants with care details to help plant owners
           find proper care for plants. By adding plants to the user list, the user can easily keep tabs on their 
           plants to determine the best care for them.
-          </p>
-        {/* <form onSubmit={this.routeToHome}>
-          <fieldset>
-            <legend>Log In</legend>
-            <label htmlFor='username'>Username</label>
-            <input name='username' id='username' required/><br/>
-            <label htmlFor='password'>Password</label>
-            <input name='password' id='password' required/><br/>
-            <p>Enter any username and password and click submit</p>
-            <button type="submit">
-                SUBMIT
-            </button>
-
-          </fieldset>
-        </form> */}
+        </p>
+        <button>
+        <Link to ="/all-plants">
+            BEGIN
+          </Link>
+        </button>
       </div>
 
     )
